dcad1a09705e5e2a3fca3d9847169d5b956a61d1
Dafasr99 <anandafa.syukur@ui.ac.id>
2023-05-25 21:18:52 +0700

    Progress TP4

--- Tugas Pemrograman 4/src/CalculationPanel.java
+++ Tugas Pemrograman 4/src/CalculationPanel.java
@@ -9,49 +9,60 @@ public class CalculationPanel extends JPanel {
     private JLabel resultLabel;
     private JLabel errorMessageLabel;
 
-    public CalculationPanel() {
+    // Constructor
+    public CalculationPanel() { 
         setLayout(new BorderLayout());
 
+        // Create labels
         JLabel infixInputLabel = new JLabel("Enter infix expression:");
         JLabel postfixLabel = new JLabel("Postfix expression:");
         resultLabel = new JLabel();
         JLabel errorLabel = new JLabel("Error message:");
 
+        // Set font
         JPanel inputLabelPanel = new JPanel(new GridLayout(4, 1));
         inputLabelPanel.add(infixInputLabel);
         inputLabelPanel.add(postfixLabel);
         inputLabelPanel.add(resultLabel);
         inputLabelPanel.add(errorLabel);
 
+        // Create text fields
         infixTextField = new JTextField();
         infixTextField.setPreferredSize(new Dimension(200, 25));
 
+        // Create text area
         postfixTextArea = new JTextArea();
         postfixTextArea.setEditable(false);
         JScrollPane postfixScrollPane = new JScrollPane(postfixTextArea);
 
+        // Create labels
         resultLabel = new JLabel();
         resultLabel.setFont(resultLabel.getFont().deriveFont(Font.BOLD, 14));
         resultLabel.setBorder(BorderFactory.createEmptyBorder(10, 0, 0, 0));
 
+        // Create error message label
         errorMessageLabel = new JLabel();
         errorMessageLabel.setForeground(Color.RED);
         errorMessageLabel.setFont(errorMessageLabel.getFont().deriveFont(Font.BOLD));
         errorMessageLabel.setBorder(BorderFactory.createEmptyBorder(10, 0, 0, 0));
 
+        // Set font
         JPanel inputValuePanel = new JPanel(new GridLayout(4, 1));
         inputValuePanel.add(infixTextField);
         inputValuePanel.add(postfixScrollPane);
         inputValuePanel.add(resultLabel);
         inputValuePanel.add(errorMessageLabel);
 
+        // Add components
         add(inputLabelPanel, BorderLayout.WEST);
         add(inputValuePanel, BorderLayout.CENTER);
 
+        // Create calculate button
         JButton calculateButton = new JButton("Calculate");
         CalculateButtonListener calculateButtonListener = new CalculateButtonListener();
         calculateButton.addActionListener(calculateButtonListener);
 
+        // Add calculate button
         add(calculateButton, BorderLayout.EAST);
     }
 
@@ -60,25 +71,34 @@ public class CalculationPanel extends JPanel {
         public void actionPerformed(ActionEvent e) {
             String infixExpression = infixTextField.getText().trim();
 
+            // Check if infix expression is empty
             if (!infixExpression.isEmpty()) {
+                // Calculate postfix expression
                 try {
+
+                    // Convert infix expression to postfix expression
                     String postfixExpression = PostfixExpression.convertToPostfix(infixExpression);
                     long result = PostfixExpression.evaluatePostfix(postfixExpression);
 
+                    // Update text area and label
                     postfixTextArea.setText(postfixExpression);
                     resultLabel.setText(String.format("Result:[ %d ]", result));
                     errorMessageLabel.setText(""); // Clear previous error message
 
-                    // Update labels
+                    // Update input label
                     JLabel infixInputLabel = (JLabel) ((JPanel) getComponent(0)).getComponent(0);
                     JLabel postfixLabel = (JLabel) ((JPanel) getComponent(0)).getComponent(1);
                     infixInputLabel.setText(String.format("Enter infix expression:[ %s ]", infixExpression));
                     postfixLabel.setText(String.format("Postfix expression:[ %s ]", postfixExpression));
+
+                // Catch exceptions
                 } catch (IllegalArgumentException ex) {
                     String errorMessage = ex.getMessage();
                     postfixTextArea.setText("");
                     resultLabel.setText("");
                     errorMessageLabel.setText(String.format("[ %s ]", errorMessage));
+                
+                // Catch arithmetic exceptions
                 } catch (ArithmeticException ex) {
                     String errorMessage = ex.getMessage();
                     postfixTextArea.setText("");
--- Tugas Pemrograman 4/src/PostfixExpression.java
+++ Tugas Pemrograman 4/src/PostfixExpression.java
@@ -3,32 +3,47 @@ import java.util.StringTokenizer;
 
 public class PostfixExpression {
     public static String convertToPostfix(String infixExpression) {
+
+        // Remove all spaces
         StringBuilder postfixExpression = new StringBuilder();
         Stack<Character> operatorStack = new Stack<>();
 
+        // Tokenize infix expression
         StringTokenizer tokenizer = new StringTokenizer(infixExpression, "+-*/^()", true);
 
+        // Convert infix expression to postfix expression
         while (tokenizer.hasMoreTokens()) {
             String token = tokenizer.nextToken().trim();
 
+            // Check if token is empty
             if (!token.isEmpty()) {
+
+                // Check if token is operand or operator
                 if (isOperand(token)) {
                     postfixExpression.append(token).append(" ");
                 } else if (isOperator(token)) {
                     char operator = token.charAt(0);
 
+                    // Check if operator is parenthesis
                     if (operator == '(') {
                         operatorStack.push(operator);
+
+                    // Check if operator is closing parenthesis
                     } else if (operator == ')') {
                         while (!operatorStack.isEmpty() && operatorStack.peek() != '(') {
                             postfixExpression.append(operatorStack.pop()).append(" ");
                         }
 
+                        // Check if operator stack is empty
                         if (!operatorStack.isEmpty() && operatorStack.peek() == '(') {
                             operatorStack.pop();
+
+                        // Check if operator stack is empty
                         } else {
                             throw new IllegalArgumentException("Invalid expression: Unbalanced parentheses");
                         }
+
+                    // Check if operator is exponentiation
                     } else {
                         while (!operatorStack.isEmpty() && getPrecedence(operator) <= getPrecedence(operatorStack.peek())) {
                             postfixExpression.append(operatorStack.pop()).append(" ");
@@ -36,48 +51,61 @@ public class PostfixExpression {
 
                         operatorStack.push(operator);
                     }
+                
                 } else {
                     throw new IllegalArgumentException("Invalid expression: Unknown token '" + token + "'");
                 }
             }
         }
 
+        // Check if operator stack is empty
         while (!operatorStack.isEmpty()) {
             if (operatorStack.peek() == '(' || operatorStack.peek() == ')') {
                 throw new IllegalArgumentException("Invalid expression: Unbalanced parentheses");
             }
 
+            // Append operator to postfix expression
             postfixExpression.append(operatorStack.pop()).append(" ");
         }
 
         return postfixExpression.toString().trim();
     }
 
+    // Evaluate postfix expression
     public static long evaluatePostfix(String postfixExpression) {
         Stack<Long> operandStack = new Stack<>();
 
+        // Tokenize postfix expression
         StringTokenizer tokenizer = new StringTokenizer(postfixExpression);
 
+        // Evaluate postfix expression
         while (tokenizer.hasMoreTokens()) {
             String token = tokenizer.nextToken();
 
+            // Check if token is operand or operator
             if (isOperand(token)) {
                 long operand = Long.parseLong(token);
                 operandStack.push(operand);
+            
+            // Check if token is operator
             } else if (isOperator(token)) {
                 if (operandStack.size() < 2) {
                     throw new IllegalArgumentException("Invalid expression: Insufficient operands");
                 }
-
+                
+                // Evaluate operation
                 long operand2 = operandStack.pop();
                 long operand1 = operandStack.pop();
                 long result = evaluateOperation(operand1, operand2, token.charAt(0));
                 operandStack.push(result);
+            
+            // Check if token is parenthesis
             } else {
                 throw new IllegalArgumentException("Invalid expression: Unknown token '" + token + "'");
             }
         }
 
+        // Check if operand stack is empty
         if (operandStack.size() != 1) {
             throw new IllegalArgumentException("Invalid expression: Too many operands");
         }
@@ -85,14 +113,17 @@ public class PostfixExpression {
         return operandStack.pop();
     }
 
+    // Check if token is operand
     private static boolean isOperand(String token) {
         return token.matches("\\d+");
     }
 
+    // Check if token is operator
     private static boolean isOperator(String token) {
         return token.matches("[+\\-*/^]");
     }
 
+    // Get precedence of operator
     private static int getPrecedence(char operator) {
         if (operator == '+' || operator == '-') {
             return 1;
@@ -105,6 +136,7 @@ public class PostfixExpression {
         }
     }
 
+    // Evaluate operation
     private static long evaluateOperation(long operand1, long operand2, char operator) {
         if (operator == '+') {
             return operand1 + operand2;
